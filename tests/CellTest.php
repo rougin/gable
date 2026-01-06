d2c893f5fef6702163f2f16ca49fc21e016eaee6
Rougin Gutib <rougingutib@gmail.com>
2026-01-07 00:47:07 +0800

    Change tests in "CellTest"

--- tests/CellTest.php
+++ tests/CellTest.php
@@ -12,7 +12,54 @@ class CellTest extends Testcase
     /**
      * @return void
      */
-    public function test_adds_badge()
+    public function test_all_props()
+    {
+        // Arrange
+        $value = 'Test Value';
+
+        $align = 'center';
+
+        $name = 'test_value';
+
+        $colspan = 2;
+
+        $rowspan = 3;
+
+        $attrs = 'align="center" class="test-class" cspan="2" rspan="3" style="color: red;" width="50%"';
+
+        // Act
+        $cell = new Cell($value, $align, 'test-class', $colspan, $rowspan, 'color: red;', 50);
+
+        // Assert
+        $actual = $cell->getColspan();
+
+        $this->assertEquals($colspan, $actual);
+
+        $actual = $cell->getValue();
+
+        $this->assertEquals($value, $actual);
+
+        $actual = $cell->getAlign();
+
+        $this->assertEquals($align, $actual);
+
+        $actual = $cell->getRowspan();
+
+        $this->assertEquals($rowspan, $actual);
+
+        $actual = $cell->getName();
+
+        $this->assertEquals($name, $actual);
+
+        $actual = $cell->getParsedAttrs();
+
+        $this->assertEquals($attrs, $actual);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_badges()
     {
         // Arrange
         $cell = new Cell;
@@ -26,9 +73,9 @@ class CellTest extends Testcase
 
         $cell->addBadge($badge2);
 
+        // Assert
         $actual = $cell->getBadges();
 
-        // Assert
         $this->assertEquals($badge1, $actual[0]);
 
         $this->assertEquals($badge2, $actual[1]);
@@ -37,7 +84,47 @@ class CellTest extends Testcase
     /**
      * @return void
      */
-    public function test_generates_name_from_value()
+    public function test_badges_as_array()
+    {
+        // Arrange
+        $cell = new Cell;
+
+        $badge1 = new Badge('Active', 'bg-success');
+
+        $badge2 = new Badge('Inactive', 'bg-danger');
+
+        $badges = array($badge1, $badge2);
+
+        // Act
+        $cell->setBadges($badges);
+
+        // Assert
+        $actual = $cell->getBadges();
+
+        $this->assertEquals($badge1, $actual[0]);
+
+        $this->assertEquals($badge2, $actual[1]);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_empty_attrs()
+    {
+        // Arrange
+        $cell = new Cell;
+
+        // Act
+        $actual = $cell->getParsedAttrs();
+
+        // Assert
+        $this->assertEmpty($actual);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_name_as_snake_case()
     {
         // Test case 1: "First Name" ---------
         // Arrange
@@ -95,74 +182,7 @@ class CellTest extends Testcase
     /**
      * @return void
      */
-    public function test_parses_all_attributes()
-    {
-        // Arrange
-        $cell = new Cell;
-
-        $cell->withAttr('id', 'my-cell');
-
-        $cell->setClass('custom-class');
-
-        $cell->setStyle('font-size: 12px;');
-
-        $cell->setWidth(75);
-
-        $expect = 'id="my-cell" class="custom-class" style="font-size: 12px;" width="75%"';
-
-        // Act
-        $actual = $cell->getParsedAttrs();
-
-        // Assert
-        $this->assertEquals($expect, $actual);
-    }
-
-    /**
-     * @return void
-     */
-    public function test_replaces_badges()
-    {
-        // Arrange
-        $cell = new Cell;
-
-        $badge1 = new Badge('Active', 'true', 'bg-success');
-
-        $badge2 = new Badge('Inactive', 'false', 'bg-danger');
-
-        $badges = array($badge1, $badge2);
-
-        // Act
-        $cell->setBadges($badges);
-
-        $actual = $cell->getBadges();
-
-        // Assert
-        $this->assertEquals($badge1, $actual[0]);
-
-        $this->assertEquals($badge2, $actual[1]);
-    }
-
-    /**
-     * @return void
-     */
-    public function test_returns_empty_attrs_for_null()
-    {
-        // Arrange
-        $cell = new Cell;
-
-        $expect = '';
-
-        // Act
-        $actual = $cell->getParsedAttrs();
-
-        // Assert
-        $this->assertEquals($expect, $actual);
-    }
-
-    /**
-     * @return void
-     */
-    public function test_sets_and_gets_name()
+    public function test_setting_name()
     {
         // Arrange
         $cell = new Cell;
@@ -181,7 +201,7 @@ class CellTest extends Testcase
     /**
      * @return void
      */
-    public function test_sets_and_gets_value()
+    public function test_setting_value()
     {
         // Arrange
         $cell = new Cell;
@@ -200,47 +220,25 @@ class CellTest extends Testcase
     /**
      * @return void
      */
-    public function test_with_all_props()
+    public function test_with_attrs()
     {
         // Arrange
-        $value = 'Test Value';
+        $cell = new Cell;
 
-        $align = 'center';
-
-        $name = 'test_value';
-
-        $colspan = 2;
-
-        $rowspan = 3;
-
-        $attrs = 'align="center" class="test-class" cspan="2" rspan="3" style="color: red;" width="50%"';
+        $expect = 'id="my-cell" class="custom-class" style="font-size: 12px;" width="75%"';
 
         // Act
-        $cell = new Cell($value, $align, 'test-class', $colspan, $rowspan, 'color: red;', 50);
+        $cell->withAttr('id', 'my-cell');
+
+        $cell->setClass('custom-class');
+
+        $cell->setStyle('font-size: 12px;');
+
+        $cell->setWidth(75);
 
         // Assert
-        $actual = $cell->getColspan();
-
-        $this->assertEquals($colspan, $actual);
-
-        $actual = $cell->getValue();
-
-        $this->assertEquals($value, $actual);
-
-        $actual = $cell->getAlign();
-
-        $this->assertEquals($align, $actual);
-
-        $actual = $cell->getRowspan();
-
-        $this->assertEquals($rowspan, $actual);
-
-        $actual = $cell->getName();
-
-        $this->assertEquals($name, $actual);
-
         $actual = $cell->getParsedAttrs();
 
-        $this->assertEquals($attrs, $actual);
+        $this->assertEquals($expect, $actual);
     }
 }
